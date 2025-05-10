70550b08b64e06b1e8231a9d0af8268144580179
Peter-J1 <peterjoseph571@gmail.com>
2025-05-10 03:19:12 -0400

    added an a counter so that all images are accounted for before drawing , more text added, and scene changed a bit.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Multimedia-Programming
 
 # CISC.3610 Portfolio (Peter.J)
+
+# Project1 Link
+http://127.0.0.1:5500/Project1/index.html
+
+# sceneProject Link
+http://127.0.0.1:5500/sceneProject/scene.html
+
+
--- sceneProject/scene.js
+++ sceneProject/scene.js
@@ -10,21 +10,38 @@ ballImage.src = "Images/168833918216354192314521-128.png";
 const netImage = new Image();
 netImage.src = "Images/3663487961636048492-128.png";
 
-
-backgroundImage.onload = function(){
+// drawImage(image, dx, dy, dwidth, dHeight);
+// fillText(text, x, y) or  fillText(text, x, y, maxWidth)
+function drawSCENE(){
     canvasContext.drawImage(backgroundImage,0,0,canvas.width, canvas.height);
+
+    canvasContext.drawImage(ballImage, 50, 350, 50, 50); // (image, foward/backwards, up/down, width, height)
+    canvasContext.drawImage(ballImage, 200, 200, 50, 50);
+    canvasContext.drawImage(ballImage, 550, 150, 50, 50);
+    canvasContext.drawImage(ballImage, 820, 100, 60, 75);
+
+    canvasContext.drawImage(netImage, 900, 70, 100, 150 );
+
+    canvasContext.font = "18px Verdana"; // size of text and styling of text
+    canvasContext.fillStyle ="yellow";
+    canvasContext.textAlign = "center";
+
+    canvasContext.fillText("GOAL!!!!", canvas.width/2,110);
+    canvasContext.fillText("What a strike from the boy wonder PT, You can still see the after images of the ball on replay", canvas.width/2,300);
+
 };
 
-ballImage.onload = function(){canvasContext.drawImage(ballImage, 0, 34, 20, 20);
+let imagesLoaded = 0;
+//making sure that all the images are loaded before drawing the scene
+function isAllImagesLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === 3)
+        drawSCENE();
 }
 
-netImage.onload = function(){canvasContext.drawImage(netImage, 900, 0, 40, 80 );
-}
+backgroundImage.onload = isAllImagesLoaded;
+ballImage.onload = isAllImagesLoaded;
+netImage.onload = isAllImagesLoaded;
 
-canvasContext.font = "24px Verdana";
-
-canvasContext.fillStyle ="yellow";
-
-canvasContext.fillText("Futbol!!!!", 200,150);
 
 
